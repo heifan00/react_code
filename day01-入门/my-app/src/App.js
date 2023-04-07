99059f18f7f3f9c56422cfbabe5dbf1fb68dfc08
HiF <heifan00@dingtalk.com>
2023-04-07 17:39:13 +0800

    请求

--- day01-入门/my-app/src/App.js
+++ day01-入门/my-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+function List() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios.post('https://df-api.yifu-tech.com/hang/system/ina/style/list', { /* 请求参数 */ })
+      .then((response) => {
+        // 校验返回数据格式是否正确
+        if (Array.isArray(response.data.data.dataList)) {
+          setList(response.data.data.dataList);
+        } else {
+          console.error('返回数据格式不正确');
+          alert('请求异常!');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://hei.fan"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HeiFan
-        </a>
-      </header>
+    <div>
+      <h2>List</h2>
+      <ul>
+        {list.map((item) => (
+          <li key={item.styleId}>
+            <a href={item.styleCollection}>{item.styleDescription}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+
+export default List;
