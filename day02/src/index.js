af27673530c3376698f58544795f3bf860f9bdad
HiF <heifan00@dingtalk.com>
2023-07-12 17:32:19 +0800

    add day02

---
+++ day02/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let title = 'test';
+let root = document.getElementById("test")
+ReactDOM.render(
+    <h1>
+        {title}
+    </h1>,
+    root
+)
